f378ed28f64d26d07503a1573d45124a1ce8f1af
Teerachai Laothong <teerachai@perfenterprise.com>
2020-02-09 23:22:36 +0700

    Added StateSvcController

---
+++ ManyFabricSolution/WebStateless/Controllers/StateSvcController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using SharedModels;
+using SimpleStateActor.Interfaces;
+using WebStateless.Models;
+
+namespace WebStateless.Controllers
+{
+    public class StateSvcController : Controller
+    {
+        private ISimpleStatefulService stateActor;
+        public StateSvcController()
+        {
+            stateActor = ServiceProxy.Create<ISimpleStatefulService>(
+                new Uri("fabric://ManyFabrics/SimpleStatefulService"), new ServicePartitionKey(1));
+        }
+
+        // GET: SimpleItems
+        public async Task<IActionResult> Index()
+        {
+            //return View(await _context.SimpleItems.ToListAsync());
+            var items = await stateActor.ListItemsAsync();
+            return View(items);
+        }
+
+        // GET: SimpleItems/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var simpleItem = await stateActor.GetItemAsync(id);
+
+            if (simpleItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(simpleItem);
+        }
+
+        // GET: SimpleItems/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SimpleItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Number")] SimpleItem simpleItem)
+        {
+            if (ModelState.IsValid)
+            {
+                await stateActor.AddItemAsync(simpleItem);
+                return RedirectToAction(nameof(Index));
+            }
+            //if (ModelState.IsValid)
+            //{
+            //    _context.Add(simpleItem);
+            //    await _context.SaveChangesAsync();
+            //    return RedirectToAction(nameof(Index));
+            //}
+            return View(simpleItem);
+        }
+
+        // GET: SimpleItems/Edit/5
+        public Task<IActionResult> Edit(string id)
+        {
+            return Details(id);
+        }
+
+        // POST: SimpleItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Number")] SimpleItem simpleItem)
+        {
+            if (id != simpleItem.Id)
+            {
+                return NotFound();
+            }
+
+            //if (ModelState.IsValid)
+            //{
+            //    try
+            //    {
+            //        _context.Update(simpleItem);
+            //        await _context.SaveChangesAsync();
+            //    }
+            //    catch (DbUpdateConcurrencyException)
+            //    {
+            //        if (!SimpleItemExists(simpleItem.Id))
+            //        {
+            //            return NotFound();
+            //        }
+            //        else
+            //        {
+            //            throw;
+            //        }
+            //    }
+            //    return RedirectToAction(nameof(Index));
+            //}
+            return View(simpleItem);
+        }
+    }
+}
